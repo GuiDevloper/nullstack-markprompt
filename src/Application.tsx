c5fb75fba6f5462b5a1b26a8596ecdbd2ab27087
Guilherme Correia <guicorrea384@gmail.com>
2023-04-07 22:37:06 -0300

    :adhesive_bandage: Add locale to html tag

--- src/Application.tsx
+++ src/Application.tsx
@@ -7,6 +7,10 @@ import { projectExists } from './utils/projects'
 
 class Application extends Nullstack {
 
+  prepare({ page }: NullstackClientContext) {
+    page.locale = 'en-US'
+  }
+
   render({ params }: NullstackClientContext) {
     return (
       <body class="bg-gray-900 text-white">
